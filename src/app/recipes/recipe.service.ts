cf043b07f8822076a8339a16c89bc589db6c8dd9
Nikolay Antonov <nikolay.antonov@globalcons.com>
2018-03-07 13:54:48 +0200

    110. Adding Ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,28 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        // tslint:disable-next-line:max-line-length
-        new Recipe('A Test Recipe', 'This is a test', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/2/29/0/0149359_Making-Taco_s4x3.jpg.rend.hgtvcom.616.462.suffix/1371603491866.jpeg'),
-        // tslint:disable-next-line:max-line-length
-        new Recipe('Another Test Recipe', 'This is a test', 'https://cook.fnr.sndimg.com/content/dam/images/cook/fullset/2012/1/26/0/Simple-Asian_Salmon_s3x4.jpg.rend.hgtvcom.616.462.suffix/1358484192681.jpeg')
+        new Recipe('Tacos',
+                   'Mexican classic',
+                   // tslint:disable-next-line:max-line-length
+                   'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/2/29/0/0149359_Making-Taco_s4x3.jpg.rend.hgtvcom.616.462.suffix/1371603491866.jpeg',
+                    [
+                        new Ingredient('Mince', 1),
+                        new Ingredient('Salad', 20)
+                    ]),
+        new Recipe('Meat with salafd',
+                   'New recipe',
+                   // tslint:disable-next-line:max-line-length
+                   'https://cook.fnr.sndimg.com/content/dam/images/cook/fullset/2012/1/26/0/Simple-Asian_Salmon_s3x4.jpg.rend.hgtvcom.616.462.suffix/1358484192681.jpeg',
+                    [
+                        new Ingredient('Meat', 2),
+                        new Ingredient('Salad', 1)
+                    ])
     ];
 
     getRecipes() {
